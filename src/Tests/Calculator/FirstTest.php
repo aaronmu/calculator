991cd751a756197c82968d6c4fdbd96fccf3cb39
Aaron Muylaert <aaron@tactics.be>
2015-07-01 21:47:38 +0200

    More robust TestTokenizer.

--- src/Tests/Calculator/FirstTest.php
+++ src/Tests/Calculator/FirstTest.php
@@ -10,20 +10,34 @@ class FirstTest extends \PHPUnit_Framework_TestCase
 {
     public function test_simple_addition()
     {
-        $m = new Math(new SimpleAdditionTokenizer(), new Dictionary());
+        $m = new Math(new TestTokenizer(['1', '+', '1']), new Dictionary());
 
         $this->assertSame(intval(2), $m->expression('foobar'));
     }
+
+    public function test_complex_addition()
+    {
+        $m = new Math(new TestTokenizer(['1', '+', '1', '+', '7', '+', '5']), new Dictionary());
+
+        $this->assertSame(intval(14), $m->expression('foobar'));
+    }
 }
 
-class SimpleAdditionTokenizer implements Tokenizer
+class TestTokenizer implements Tokenizer
 {
+    private $tokens;
+
+    public function __construct(array $tokens)
+    {
+        $this->tokens = $tokens;
+    }
+
     /**
      * @param $str
      * @return array
      */
     public function tokenize($str)
     {
-        return ['1', '+', '1'];
+        return $this->tokens;
     }
 }
