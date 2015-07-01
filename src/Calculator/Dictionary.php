fef628026c0b9303a6e6f25cf3a5d8632aeaf367
Aaron Muylaert <aaron@tactics.be>
2015-07-01 21:28:35 +0200

    Move files around, remove unsupported operators.

--- src/Calculator/Dictionary.php
+++ src/Calculator/Dictionary.php
@@ -2,15 +2,13 @@
 
 namespace Calculator;
 
+use Calculator\Operator\Addition;
+
 class Dictionary
 {
     public static function fromString($str)
     {
         if (is_numeric($str)) return new Number($str);
         if ($str === '+') return new Addition($str);
-        if ($str === '-') return new Subtraction($str);
-        if ($str === '*') return new Multiplication($str);
-        if ($str === '/') return new Division($str);
-        if (in_array($str, ['(', ')'])) return new Parenthesis($str);
     }
 }
--- src/Calculator/Division.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Calculator;
-
-
-class Division implements ExpressionPart
-{
-
-}
